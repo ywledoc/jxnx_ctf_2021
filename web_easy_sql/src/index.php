b581f6a8dad479106eb2df16d8bd5d4465b3c0e8
ywledoc <ywledoc@outlook.com>
2021-03-09 08:55:11 +0800

    增加Reverse题目/web_easy_sql修复

--- web_easy_sql/src/index.php
+++ web_easy_sql/src/index.php
@@ -1,22 +1,22 @@
 <?php
-$con = mysql_connect("127.0.0.1","root","root");
+$con = mysqli_connect("127.0.0.1","root","root");
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
   }
 
-mysql_select_db("supersqli", $con);
+mysqli_select_db("supersqli", $con);
 
 if(isset($_GET)){
 	$_GET['id'] = 1;
 }
-$result = mysql_query("SELECT * FROM words where id=".$_GET['id']);
+$result = mysqli_query("SELECT * FROM words where id=".$_GET['id']);
 
-while($row = mysql_fetch_array($result))
+while($row = mysqli_fetch_array($result))
   {
   echo $row['data'];
   echo "<br />";
   }
 
-mysql_close($con);
+mysqli_close($con);
 ?>
